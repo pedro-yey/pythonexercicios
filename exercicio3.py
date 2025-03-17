6f2daa92b31b284f09f699ec5ed689d2af011aa6
pedroh_yeyy <admdogatinbonito@gmail.com>
2025-03-17 19:30:08 -0300

    Add files via upload

---
+++ exercicio3.py
@@ -0,0 +1,5 @@
+n1=float(input('me fale um número'))
+n2=float(input('me fale outro número'))
+s=n1+n2
+print('A soma entre {} e {} equivale a {}' .format(n1, n2, s))
+#Exercício da Aula 06, usei o .format para ser mais rápido nos meus códigos.
