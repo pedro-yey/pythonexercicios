abc55b244081e5ad2053004cddb7ed7e8df257f3
pedroh_yeyy <admdogatinbonito@gmail.com>
2025-03-17 19:30:57 -0300

    Add files via upload

---
+++ exercicio5.py
@@ -0,0 +1,3 @@
+n1=int(input('Me fale um número'))
+print('O antecessor de seu número é {} e seu sucessor {}' .format (n1-1, n1+1))
+#Exercicio 5 da aula 07, o exercício consiste em falar um número e em seguida seu antecessor e sucessor.
