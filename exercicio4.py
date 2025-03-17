b3ecc69b6953bc466b3530b755be7b625b74dadc
pedroh_yeyy <admdogatinbonito@gmail.com>
2025-03-17 19:30:38 -0300

    Add files via upload

---
+++ exercicio4.py
@@ -0,0 +1,7 @@
+sla=input('digite algo')
+print (type(sla))
+print(sla.isalpha())
+print(sla.isnumeric())
+print(sla.islower())
+print(sla.isspace())
+#Aula 06 do Curso em vídeo, exercício para saber o tipo primitivo e depois se é numerico, alfanumérico e etc.
